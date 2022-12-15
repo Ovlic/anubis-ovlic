0349be4b0966e11890eae24b69669d4aba3c6e0c
Ovlic <74332950+Ovlic@users.noreply.github.com>
2022-12-15 11:51:10 -0800

    added encrypt and decrypt to main

---
+++ ovlic_anubis/__init__.py
@@ -0,0 +1,5 @@
+
+__version__ = "0.0.1"
+__author__ = "ovlic"
+
+from crypt_backend import encrypt, decrypt
---
+++ ovlic_anubis/crypt_backend.py
@@ -0,0 +1,12 @@
+import base64, os, utils
+import ancrypt
+
+
+def decrypt(ciphertext:str):
+    return ancrypt.load(ciphertext, text=True)
+
+def encrypt(plaintext:str):
+    _input = utils.carbon(plaintext)
+    key = base64.b64encode(os.urandom(32)).decode()
+    _input = utils.Encryption(key.encode(), text=True).write(key, _input)
+    return _input
